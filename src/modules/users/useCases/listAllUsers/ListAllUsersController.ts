09f408150e4149a3d8ccc6104d78e16e811a32d5
Al3xsandro <alexs123655@gmail.com>
2021-12-04 15:43:28 -0300

    feat: challenge finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      const users = this.listAllUsersUseCase.execute({ user_id } as {
+        user_id: string;
+      });
+
+      return response.status(200).send(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,17 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      const userProfile = this.showUserProfileUseCase.execute({ user_id } as {
+        user_id: string;
+      });
+
+      return response.status(200).json(userProfile);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
